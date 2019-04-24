ab886d5fd3ef28a4ed34c193cebe6e8b0aa48dfe
bigusdeveloper <bigusdeveloper@gmail.com>
2019-04-24 02:49:49 +0000

    Drafting up plan for db implementation

---
+++ draft.py
@@ -0,0 +1,17 @@
+# TODO - Download NVDI data for date/time range
+
+# TODO - Create database file
+
+# TODO - Iterate through NVDI JSON and move data to database
+
+# TODO -
+
+# TODO -
+
+# TODO -
+
+# TODO -
+
+# TODO -
+
+# TODO -
